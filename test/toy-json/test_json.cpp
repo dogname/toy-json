6cf0dc41eb125876382b3dcd94143e2d07305978
ticks <xiehuiwu1996@gmail.com>
2019-10-27 17:27:41 +0800

    update string test

--- test/toy-json/test_json.cpp
+++ test/toy-json/test_json.cpp
@@ -83,16 +83,36 @@ TEST(Value, PARSE_INVILID)
     EXPECT_EQ(PARSE_INVALID_VALUE, v.parse("fales"));
 }
 
+inline bool stringEQ(const char* expect, const char* json, size_t length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        if (expect[i] != json[i]) return false;
+    }
+    return true;
+}
+
+#define TEST_STRING(expect, json)                                       \
+    do                                                                  \
+    {                                                                   \
+        EXPECT_EQ(PARSE_OK, v.parse(json));                             \
+        EXPECT_EQ(JSON_STRING, v.getType());                            \
+        EXPECT_TRUE(stringEQ(expect, v.getString(), v.getStrLength())); \
+    } while (0)
+
 TEST(Value, PARSE_STRING)
 {
     using namespace TOYJSON;
     Value v;
-    EXPECT_EQ(PARSE_OK, v.parse("\"hello, world\""));
-    EXPECT_STREQ("hello, world", v.getString());
-    EXPECT_EQ(PARSE_OK, v.parse("\"\""));
-    EXPECT_STREQ("", v.getString());
-    EXPECT_EQ(PARSE_OK, v.parse("\"hello\\nworld\""));  // 转意字符
-    EXPECT_STREQ("hello\nworld", v.getString());
-    EXPECT_EQ(PARSE_OK, v.parse("\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\""));
-    EXPECT_STREQ("\" \\ / \b \f \n \r \t", v.getString());
+    TEST_STRING("hello, world", "\"hello, world\"");
+    TEST_STRING("", "\"\"");
+    TEST_STRING("hello\nworld", "\"hello\\nworld\"");
+    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+
+    TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
+    TEST_STRING("\x24", "\"\\u0024\"");                    /* Dollar sign U+0024 */
+    TEST_STRING("\xC2\xA2", "\"\\u00A2\"");                /* Cents sign U+00A2 */
+    TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\"");            /* Euro sign U+20AC */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\""); /* G clef sign U+1D11E */
+    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\""); /* G clef sign U+1D11E */
 }
